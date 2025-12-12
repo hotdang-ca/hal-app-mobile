72a22960b74c8eaaa6cba355216257a18aabed32
James Robert Perih <james@hotdang.ca>
2025-12-12 08:26:42 -0600

    Protect secrets.

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -3,12 +3,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBpvrpPAkMfc1oTMZYToZumeDd5FXMb0JQ",
-    authDomain: "halapp-15095.firebaseapp.com",
-    projectId: "halapp-15095",
-    storageBucket: "halapp-15095.firebasestorage.app",
-    messagingSenderId: "403774189786",
-    appId: "1:403774189786:web:583891b7de30bba069b5e3",
+    apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
